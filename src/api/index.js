8c2e776b949fc98cd5e00cc7b9d878a69c7c70ab
KarryCMT <1570606465@qq.com>
2023-01-15 13:53:47 +0800

    chore: 删除不必要的代码

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,7 @@
 import http from '@/utils/request';
 
-// 用户注册
-const UserRegister = (params) => http.post('/user/register', params);
-// 用户登录
-const UserLogin = (params) => http.post('/user/login', params);
-
+// 测试
+const Test = (params) => http.post('/user/test', params);
 export {
-  UserRegister,
-  UserLogin
+  Test
 };
